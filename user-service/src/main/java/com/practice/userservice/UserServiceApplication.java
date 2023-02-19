c2d490474257f7e2ef8681f94f8d00ff560f835b
georgi888111@gmail.com <georgina.crespo@globallogic.com>
2023-02-19 12:58:08 -0300

    Implementación de Eureka a los microservicios

--- user-service/src/main/java/com/practice/userservice/UserServiceApplication.java
+++ user-service/src/main/java/com/practice/userservice/UserServiceApplication.java
@@ -2,11 +2,13 @@ package com.practice.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
