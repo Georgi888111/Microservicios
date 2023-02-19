14a5c68f9045133866f79cb9c475fd3a44266cb5
georgi888111@gmail.com <georgina.crespo@globallogic.com>
2023-02-19 13:47:51 -0300

    Feign client, remove url

--- user-service/src/main/java/com/practice/userservice/feignclient/CarFeignClient.java
+++ user-service/src/main/java/com/practice/userservice/feignclient/CarFeignClient.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 
-@FeignClient(name = "car-service" , url = "http://localhost:8002/car")
+@FeignClient(name = "car-service")
 public interface CarFeignClient {
 
     @PostMapping()
